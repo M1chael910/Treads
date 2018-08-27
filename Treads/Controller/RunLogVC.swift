8234dda8c8a5910504ff8e0e4763970290ae3f98
Michael Murphy <mpmurphy1432@gmail.com>
2018-08-27 17:33:51 -0500

    Current run VC done

--- Treads/Controller/RunLogVC.swift
+++ Treads/Controller/RunLogVC.swift
@@ -12,7 +12,8 @@ class RunLogVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
 
