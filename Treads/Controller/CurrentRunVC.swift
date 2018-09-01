0681f2c775a0045561ff128ba574ce42e3ec9d1d
Michael Murphy <mpmurphy1432@gmail.com>
2018-08-31 21:26:15 -0500

    stuff

---
+++ Treads/Controller/CurrentRunVC.swift
@@ -0,0 +1,50 @@
+//
+//  CurrentRunVC.swift
+//  Treads
+//
+//  Created by Michael  Murphy on 8/30/18.
+//  Copyright © 2018 Michael  Murphy. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: LocationVC {
+
+    @IBOutlet weak var swipeBGImageView: UIImageView!
+    @IBOutlet weak var sliderImageVIew: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let swipeGesture = UIPanGestureRecognizer(target: self, action: #selector(endRunSwiped(sender:)))
+        sliderImageVIew.addGestureRecognizer(swipeGesture)
+        sliderImageVIew.isUserInteractionEnabled = true
+        swipeGesture.delegate = self as? UIGestureRecognizerDelegate
+    }
+    
+    @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
+        let minAdjust: CGFloat = 80
+        let maxAdjust: CGFloat = 128
+        if let sliderView = sender.view {
+            if sender.state == .began || sender.state == .changed {
+                let translation = sender.translation(in: self.view)
+                if sliderView.center.x >= (swipeBGImageView.center.x - minAdjust) && sliderView.center.x <= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = sliderView.center.x + translation.x
+                } else if sliderView.center.x >= (swipeBGImageView.center.x + maxAdjust) {
+                    sliderView.center.x = swipeBGImageView.center.x + maxAdjust
+                    //End Run code goes
+                    dismiss(animated: true, completion: nil)
+                } else {
+                    sliderView.center.x = swipeBGImageView.center.x - minAdjust
+                }
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            } else if sender.state == .ended {
+                UIView.animate(withDuration: 0.1) {
+                    sliderView.center.x = self.swipeBGImageView.center.x - minAdjust
+                }
+                
+            }
+        }
+    }
+    
+    
+}
